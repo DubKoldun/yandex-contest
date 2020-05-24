75ddd9f16d992796356a2ab01c9039e2ed3cd5de
DubKoldun <vitaliy.epifantsev@yandex.ru>
2020-05-24 20:48:38 +0300

    Add gtk support

--- default.nix
+++ default.nix
@@ -1,6 +1,9 @@
 with import <nixpkgs> {};
+let
+  opencvGtk = opencv.override (old : { enableGtk2 = true; });
+in
 stdenv.mkDerivation {
-    name = "dev-environment"; # Probably put a more meaningful name here
+    name = "ocr";
     src = ./.;
-    buildInputs = [ pkgconfig zlib cmake leptonica opencv tesseract4 gcc ];
+    buildInputs = [ pkgconfig zlib cmake leptonica opencvGtk tesseract4 gcc ];
 }
