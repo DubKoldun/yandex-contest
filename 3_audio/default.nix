c60b878883c6f50b1cc95e6783310b888d9199a1
DubKoldun <vitaliy.epifantsev@yandex.ru>
2020-07-10 17:52:15 +0300

    add find peaks

--- 3_audio/default.nix
+++ 3_audio/default.nix
@@ -3,5 +3,5 @@ with pkgs.python3Packages;
 buildPythonPackage rec {
   name = "mypackage";
   src = ./.;
-  propagatedBuildInputs = [ pyaudio pydub pytest numpy ];
+  propagatedBuildInputs = [ python38Packages.numba pyaudio pydub pytest librosa matplotlib  python38Packages.librosa python38Packages.numpy ];
 }
--- 3_audio/py_solution/script.py
+++ 3_audio/py_solution/script.py
@@ -1,16 +1,42 @@
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
 
+import librosa
+import datetime
+import numpy as np
+
 sound = AudioSegment.from_wav("/home/vutaliy/Downloads/task.wav")
-# sound = AudioSegment.from_file("sound1.wav")
 
+# morseInternational = {"."}
+
+# Split on loud parts
 chunks = split_on_silence(sound,
-    # must be silent for at least half a second
-    min_silence_len=500,
+    # Must be silent for at least 0.2 of a second
+    min_silence_len=200,
 
-    # consider it silent if quieter than -16 dBFS
-    silence_thresh=-28
+    # Consider it silent if quieter than -40 dBFS
+    silence_thresh=-40
 )
 
 for i, chunk in enumerate(chunks):
-    chunk.export("/home/vutaliy/prod/yandex_contest/3_audio/cpp_solution/chunk{0}.wav".format(i), format="wav")
+    # Export
+    chunk.export("/home/vutaliy/prod/yandex_contest/3_audio/py_solution/buff/chunk{0}.wav".format(i), format="wav")
+    
+    # Get file duration in seconds
+    y, sr = librosa.core.load("/home/vutaliy/prod/yandex_contest/3_audio/py_solution/buff/chunk{0}.wav".format(i))
+    y_mono = librosa.to_mono(y)
+
+    # Duration in seconds
+    duration = librosa.get_duration(y)
+    print ("{0}".format(i))
+    print("File duration(s): ", str(datetime.timedelta(seconds=duration)))
+
+    # Find peaks
+    onset_env = librosa.onset.onset_strength(y=y_mono, sr=sr,
+                                            hop_length=512,
+                                            aggregate=np.median, fmax=8000, n_mels=256)
+    peaks = librosa.util.peak_pick(onset_env, 3, 3, 3, 5, 5, 10)
+    print('Peaks detected at: ', librosa.frames_to_time(peaks, sr=sr))
+
+
+h
