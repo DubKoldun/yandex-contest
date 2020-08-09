04e5fdd51de6df8064b2d4b252b4e6a9870ab48b
DubKoldun <vitaliy.epifantsev@yandex.ru>
2020-08-09 07:19:16 +0300

    Task 3. Done. Update some comments

--- 3_audio/py_solution/script.py
+++ 3_audio/py_solution/script.py
@@ -5,7 +5,7 @@ import librosa
 import datetime
 import numpy as np
 
-sound = AudioSegment.from_wav("/home/vutaliy/Downloads/task.wav")
+sound = AudioSegment.from_wav("direction/file.wav")
 
 morseInternational = {
     ".-"   : 'A',
@@ -68,10 +68,10 @@ morseInternational = {
 
 # Split on loud parts
 chunks = split_on_silence(sound,
-    # Must be silent for at least 0.2 of a second
+    # Must be silent for at least 0.5 of a second
     min_silence_len=500,
 
-    # Consider it silent if quieter than -40 dBFS
+    # Consider it silent if quieter than -45 dBFS
     silence_thresh=-45
 )
 
@@ -79,10 +79,10 @@ ans = ""
 
 for i, chunk in enumerate(chunks):
     # Export
-    chunk.export("/home/vutaliy/prod/yandex_contest/3_audio/py_solution/buff/chunk{0}.wav".format(i), format="wav")
+    chunk.export("path_to_chunks/chunk{0}.wav".format(i), format="wav")
     
     # Get file duration in seconds
-    y, sr = librosa.core.load("/home/vutaliy/prod/yandex_contest/3_audio/py_solution/buff/chunk{0}.wav".format(i))
+    y, sr = librosa.core.load("path_to_chunks/chunk{0}.wav".format(i))
     y_mono = librosa.to_mono(y)
 
     # Duration in seconds
@@ -90,17 +90,19 @@ for i, chunk in enumerate(chunks):
     print ("{0}".format(i))
     print("File duration(s): ", str(duration))
 
-    # Find peaks ищи пики ручками !!! TODO()
+    # Find peaks 
     onset_env = librosa.onset.onset_strength(y=y_mono, sr=sr,
                                             hop_length=512,
                                             aggregate=np.median, fmax=8000, n_mels=256)
     peaks = librosa.util.peak_pick(onset_env, 3, 3, 3, 5, 5, 10)
 
+    # Find time peaks
     times = librosa.frames_to_time(peaks, sr=sr)
     prev = times[0]
     last = prev
     s = ""
 
+    # Convert duration between peaks to Morse code
     for i, some in enumerate(times):
         if i != 0:
             if some - prev < 0.6: 
@@ -114,8 +116,12 @@ for i, chunk in enumerate(chunks):
     else:
         s = s + '-' 
     
+    # Convert code of one Morse code symbol to one international symbol 
     print('Peaks detected at: ', librosa.frames_to_time(peaks, sr=sr))
     print (morseInternational[s])
+
+    # Save answer
     ans = ans + s + " "
 
+# Print all answer
 print (ans)
